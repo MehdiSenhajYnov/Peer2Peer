1f7d3228a6070217b03d0c4c235155975c195a62
MehdiSenhajYnov <mehdi.senhaj@ynov.com>
2022-11-13 20:41:36 +0100

    commit

--- ProjectOne/Program.cs
+++ ProjectOne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 
@@ -6,14 +7,18 @@ namespace TcpPeer2Peer
 {
     class Peer
     {
+        public static string _ipAddress = String.Empty;
         public static void Main(string[] args)
         {
-            Console.WriteLine("Starting Peer ...");
             // my public ip (portable pc) = "77.205.68.255"
             // other side public ip (home pc) = "176.150.133.69"
+            _ipAddress = File.ReadAllText("ip.txt");
+
+            Console.WriteLine("Starting Peer, trying to connect to: " + _ipAddress);
+
             TcpClient client = new TcpClient();
 
-            client.Connect(new IPEndPoint(IPAddress.Parse("77.205.68.255"), 7777));
+            client.Connect(new IPEndPoint(IPAddress.Parse(_ipAddress), 7777));
             
             while (true)
             {
