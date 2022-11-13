7202216c3f2f3608b93397c3dbe04a4917f47d71
MehdiSenhajYnov <mehdi.senhaj@ynov.com>
2022-11-13 20:51:31 +0100

    commit

--- ProjectOne/Program.cs
+++ ProjectOne/Program.cs
@@ -8,24 +8,33 @@ namespace TcpPeer2Peer
     class Peer
     {
         public static string _ipAddress = String.Empty;
+        public static TcpClient client = new TcpClient();
+
         public static void Main(string[] args)
         {
             // my public ip (portable pc) = "77.205.68.255"
             // other side public ip (home pc) = "176.150.133.69"
             _ipAddress = File.ReadAllText("ip.txt");
 
-            Console.WriteLine("Starting Peer, trying to connect to: " + _ipAddress);
-
-            TcpClient client = new TcpClient();
-
-            client.Connect(new IPEndPoint(IPAddress.Parse(_ipAddress), 7777));
+            Console.WriteLine("Starting Peer ...");
             
-            while (true)
+        }
+
+        public static async void HolePunching()
+        {
+            Console.WriteLine("Trying to connect to: " + _ipAddress);
+            
+            await client.ConnectAsync(new IPEndPoint(IPAddress.Parse(_ipAddress), 7777));
+            if (client.Connected)
             {
-                if (client.Connected)
+                Console.WriteLine("Connected");
+                while (true)
                 {
-                    Console.WriteLine("Connected");
+                    
                 }
+            } else
+            {
+                HolePunching();
             }
         }
     }
