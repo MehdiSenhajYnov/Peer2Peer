db18fa521a70d56b2df509e1e5707ad3382dce56
MehdiSenhajYnov <mehdi.senhaj@ynov.com>
2022-11-13 20:25:20 +0100

    commit

---
+++ ProjectOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpPeer2Peer
+{
+    class Peer
+    {
+        public static void Main(string[] args)
+        {
+           
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(new IPEndPoint(IPAddress.Parse("77.205.68.255"), 7777));
+
+            while (true)
+            {
+                if (socket.Connected) {
+                    Console.WriteLine("Connected");
+                }
+            }
+        }
+    }
+}
