bc5c70718eb1524b2780f9121491d9364927fc24
MehdiSenhajYnov <mehdi.senhaj@ynov.com>
2022-11-19 01:28:03 +0100

    commit

--- Server/HolePunching.cs
+++ Server/HolePunching.cs
@@ -17,7 +17,7 @@ namespace TcpPeer2PeerServer
         {
             new HolePunching()
             {
-                localEndPoint = new IPEndPoint(IPAddress.Parse("LOCAL_IP"), 1234),
+                localEndPoint = new IPEndPoint(IPAddress.Parse("20.13.17.73"), 1234),
                 remoteEndPoint = (IPEndPoint)newRemoteEndPoint,
                 useParallelAlgorithm = true,
             }.RunImpl();
